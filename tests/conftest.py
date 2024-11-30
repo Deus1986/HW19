d08026e7a38e91d4174d3695453bdfc365748db2
Vladimir Krinitsyn <Premio1896@rambler.ru>
2024-11-30 05:29:09 +0300

    15

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
+import allure
 import allure_commons
 import pytest
+import requests
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.options.ios import XCUITestOptions
@@ -60,4 +62,32 @@ def mobile_management():
 
     yield
 
+    allure.attach(
+        browser.driver.get_screenshot_as_png(),
+        name='Screenshot',
+        attachment_type=allure.attachment_type.PNG
+    )
+
+    allure.attach(
+        browser.driver.page_source,
+        name='Page source XML',
+        attachment_type=allure.attachment_type.XML
+    )
+
+    session_id = browser.driver.session_id
+
     browser.quit()
+
+    browser_stack_session = requests.get(f'https://api-cloud.browserstack.com/app-automate/sessions/{session_id}.json',
+                                         auth=(config.user_name, config.access_key)).json()
+    video_url = browser_stack_session['automation_session']['video_url']
+
+    allure.attach(
+        "<html><body>"
+        "<video width='100%' height='100%' controls autoplay>"
+        f"<source src='{video_url}' type='video/mp4'>"
+        "</video>"
+        "</body></html>",
+        name='mobile video',
+        attachment_type=allure.attachment_type.HTML
+    )
